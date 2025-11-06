7a8472d0d73635393c2409ac9475d8d44b1a2dc5
filemakrxpert <filemakrxpert@gmail.com>
2025-11-06 17:55:26 +0530

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
+
+router.get('/admin', (req, res) => {
   if (req.session && req.session.admin) {
     return res.redirect('/admin/dashboard')
   }
@@ -9,6 +10,26 @@ router.get('/', (req, res) => {
 })
 
 
+router.get('/',(req,res)=>{
+  res.render('index',{layout: false})
+})
+
+
+router.get('/about',(req,res)=>{
+  res.render('about',{layout: false})
+})
+
+
+
+router.get('/programs',async (req,res)=>{
+     const [rows] = await req.db.query('SELECT * FROM categories ')
+  res.render('services',{layout: false,rows})
+})
+
+
+router.get('/contact',(req,res)=>{
+  res.render('contact',{layout: false})
+})
 
 // payment/razorpay-success
 
